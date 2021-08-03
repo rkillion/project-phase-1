7dd8f77f070fef557e3a3d42923929aa500d2c35
robertfkillion@gmail.com <robertfkillion@gmail.com>
2021-08-03 16:25:23 -0400

    implemented functions and css

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 document.addEventListener("DOMContentLoaded",()=> {
-
+    document.getElementById("images-request-form").addEventListener("submit",e=>{
+        e.preventDefault();
+        document.getElementById("gallery-area").innerHTML = '';
+        getPixaPhotos(e.target.topic.value);
+    })
 })
 
 //-----Write a function to get photographs from the api
 
 function getPixaPhotos(topic) {
-    fetch(`https://pixabay.com/api/?key=${key}&q=${topic}&image_type=photo`).then(r=>r.json()).then(testRender);
+    fetch(`https://pixabay.com/api/?key=${key}&q=${topic}&image_type=photo`).then(r=>r.json()).then(j=>j.hits.forEach(renderPhotoPost));
 }
 
 
@@ -14,6 +18,17 @@ function getPixaPhotos(topic) {
 function testRender(photoData) {
     let photo = document.createElement("img");
     photo.src = photoData.hits[0].webformatURL;
+    console.log(photo.src);
     document.getElementById("gallery-area").append(photo);
+}
+
+function renderPhotoPost(photoData){
+    let photoPost = document.createElement("div");
+    photoPost.id = `post${photoData.id}`;
+    photoPost.className = "photo-post";
+    let photo = document.createElement("img");
+    photo.src = photoData.webformatURL;
+    photoPost.append(photo);
+    document.getElementById("gallery-area").append(photoPost);   
 }
 
