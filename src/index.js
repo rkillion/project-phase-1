e266d6e0fe489cf429ae5f8c40ace3085b4df246
robertfkillion@gmail.com <robertfkillion@gmail.com>
2021-08-03 09:34:04 -0400

    added functions for getting photos

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 document.addEventListener("DOMContentLoaded",()=> {
-    console.log(key);
+
 })
 
 //-----Write a function to get photographs from the api
 
+function getPixaPhotos(topic) {
+    fetch(`https://pixabay.com/api/?key=${key}&q=${topic}&image_type=photo`).then(r=>r.json()).then(testRender);
+}
+
 
 //-----Write a function that renders the photograph data on the page
+
+function testRender(photoData) {
+    let photo = document.createElement("img");
+    photo.src = photoData.hits[0].webformatURL;
+    document.getElementById("gallery-area").append(photo);
+}
 
